3314c288b1dca19c8655c96c90ce905172924c51
Valdemar_S <valdemarstidsen@gmail.com>
2025-11-07 11:37:41 +0100

    Moving Gates and Platforms

---
+++ RoboFish/Assets/MovingPlatform.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float movespeed = 2;
+
+    private Vector3 nextPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nextPosition = pointB.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, movespeed * Time.deltaTime);
+        if (transform.position == nextPosition)
+        {
+            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
