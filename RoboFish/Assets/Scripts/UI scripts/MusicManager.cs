d5747ccb7a807070dce60a218af8ccce719a5ea0
hadziemin <emin300804@gmail.com>
2025-12-05 00:29:12 +0100

    MusicManager
    MusicManager script/gameobjects added. NOTE: NOT A PREFAB YET

---
+++ RoboFish/Assets/Scripts/UI scripts/MusicManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioMixerGroup musicMixer;
+
+    public AudioSource melodySource;
+    public AudioSource drumsSource;
+
+
+    public bool enableDrums = true;
+
+    private void Awake()
+    {
+        if (melodySource.isPlaying)
+        {
+            melodySource.Play();
+        }
+
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            enableDrums = false;
+        }
+
+        UpdateDrums();
+    }
+
+    public void Update()
+    {
+        if (Time.timeScale == 0)
+        {
+            if (drumsSource.isPlaying)
+            {
+                drumsSource.Pause();
+            }
+        }
+        else
+        {
+            UpdateDrums();
+        }
+    }
+
+
+
+    public void UpdateDrums() //tjekker og sikrer at drums og melodi er in time
+    {
+        if(enableDrums = true)
+        {
+            if (!drumsSource.isPlaying)
+            {
+                drumsSource.timeSamples = melodySource.timeSamples;
+                drumsSource.Play();
+            }
+        }
+        else
+        {
+            if (drumsSource.isPlaying)
+            {
+                drumsSource.Pause();
+            }
+        }
+    }
+
+
+
+    public void EnableDrums()
+    {
+        enableDrums = true;
+        UpdateDrums();
+    }
+
+    public void DisableDrums()
+    {
+        enableDrums = false;
+        UpdateDrums();
+    }
+
+
+}
