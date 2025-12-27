d9e0960aeb314a45fc6a1a47b14ae7767af3e697
Lalacska <laura.mezei@gmail.com>
2025-12-27 12:39:07 +0100

    New fish collider to try
    New fish collider to test
    Added comments to load time, and added reset all time for editor mode

--- RoboFish/Assets/Scripts/UI scripts/LoadSavedTime.cs
+++ RoboFish/Assets/Scripts/UI scripts/LoadSavedTime.cs
@@ -9,15 +9,26 @@ public class LoadSavedTime : MonoBehaviour
     [SerializeField]
     List<GameObject> levels;
 
+    [SerializeField]
+    bool resetTimes = false;
+
     string levelName;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+#if (UNITY_EDITOR)
+        if (resetTimes)
+        {
+            TimeReset();
+        }
+#endif
         TimeSetting();
-        //TimeReset();
+
     }
 
+    // Goes trough the levels to load the saved time for each of them
     public void TimeSetting()
     {
         foreach (var level in levels)
@@ -29,6 +40,8 @@ public class LoadSavedTime : MonoBehaviour
         }
     }
 
+
+    // Finds the time and loads it in to the right text element
     public void LoadTime(TMP_Text TimeText)
     {
         if (levelName != null)
@@ -41,19 +54,20 @@ public class LoadSavedTime : MonoBehaviour
             }
             else
             {
-                TimeText.text = "00:00"; 
+                TimeText.text = "00:00";
                 PlayerPrefs.SetString(levelName, $"00:00");
             }
         }
     }
 
+    // Reset times for every level
     public void TimeReset()
     {
         foreach (var level in levels)
         {
             TMP_Text TimeText = level.GetComponentInChildren<TMP_Text>();
             levelName = level.name;
-            TimeText.text = "--:--";
+            TimeText.text = "00:00";
             PlayerPrefs.SetString(levelName, $"00:00");
         }
     }
